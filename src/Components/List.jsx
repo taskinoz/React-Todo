9c5f9cd2855d4aa4a87cc644e446a814bbc1ba58
Tristan Kennedy <tristan@taskinoz.com>
2022-08-23 21:36:45 +1000

    Updated list with checks

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import styled from "styled-components";
+
+const List = styled.div`
+  
+`
+
+const ListChecked = styled(Form.Check)`
+  ${props => props.isChecked && 'label {text-decoration: line-through;}'}
+`
+
 const ToDoList = ({
     list,
     updateList,
 }) => {
+  const listItems = Object.keys(list);
+  const [ticked, setTicked] = useState([])
+
+  const updateTicked = (item, index, remove=false) => {
+    let newList = [...ticked];
+    remove ? newList.splice(index, remove) : newList.splice(index, remove, item);
+    setTicked(newList);
+  }
+
   return (
-    (Object.keys(list)).map(item =>
-      <p>{list[item]} <span onClick={() => updateList(item,true)}>x</span></p>  
-    )
+    <List>
+      {listItems === 0 && 
+        <div className="empty">
+          <p>List Empty</p>
+        </div>
+      }
+      {listItems.length > 0 && listItems.map((item, index) =>
+        <div className="list-item">
+          <ListChecked
+            type='checkbox'
+            label={list[item]}
+            onChange={(e) => updateTicked(item, index, e.target.checked)}
+            isChecked={ticked.includes(item)}
+          />
+          <span onClick={() => {
+            updateList(item,true);
+            updateTicked(item, index, true);
+          }}>x</span>
+        </div>  
+      )}
+    </List>
+    
   );
 }
 
